d7ea0cfe1901a77f21dc374a22e41ef67553eb8a
sarakmenon <sarakmenon@gmail.com>
2025-12-23 18:09:49 -0600

    robots

---
+++ app/robots.ts
@@ -0,0 +1,11 @@
+import { MetadataRoute } from 'next';
+
+export default function robots(): MetadataRoute.Robots {
+  return {
+    rules: {
+      userAgent: '*',
+      allow: '/',
+    },
+    sitemap: 'https://math-101.com/sitemap.xml',
+  };
+}
